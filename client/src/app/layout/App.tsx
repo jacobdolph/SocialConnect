bcb0e34bd02678f5887937a0923812d6e36ce8be
Jacob Dolph <jacob.dolph702@gmail.com>
2020-09-16 15:53:47 -0600

    updated the C# crud operations, created a navbar for the front end

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
+import { Container, Header, Icon, List } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
+import NavBar from "../../features/Nav";
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  return (
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default App;

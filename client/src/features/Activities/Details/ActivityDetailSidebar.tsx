b586fe350db20540eeb92b66b722eda0794d6785
Jacob Dolph <jacob.dolph702@gmail.com>
2020-10-22 18:40:24 -0600

    added attendees details to the detailed view of activities

--- client/src/features/Activities/Details/ActivityDetailSidebar.tsx
+++ client/src/features/Activities/Details/ActivityDetailSidebar.tsx
@@ -1,8 +1,16 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { Segment, List, Item, Label, Image } from "semantic-ui-react";
+import { IAttendee } from "../../../app/models/activity";
 
-const ActivityDetailSidebar = () => {
+interface IProps {
+  attendees: IAttendee[];
+}
+
+const ActivityDetailSidebar: React.FC<IProps> = ({attendees}) => {
+
+const isHost = false;
+
   return (
     <Fragment>
       <Segment
@@ -13,45 +21,30 @@ const ActivityDetailSidebar = () => {
         inverted
         color="teal"
       >
-        3 People Going
+        {attendees.length} {attendees.length === 1 ? 'Person': "People"} going
       </Segment>
       <Segment attached>
         <List relaxed divided>
-          <Item style={{ position: "relative" }}>
+          {attendees.map((attendee)=>(
+            <Item key={attendee.username} style={{ position: "relative" }}>
+              {isHost && 
             <Label
               style={{ position: "absolute" }}
               color="orange"
               ribbon="right"
             >
               Host
-            </Label>
-            <Image size="tiny" src={"/assets/user.png"} />
+            </Label>}
+            <Image size="tiny" src={attendee.image || "/assets/user.png"} />
             <Item.Content verticalAlign="middle">
               <Item.Header as="h3">
-                <Link to={`#`}>Bob</Link>
+              <Link to={`/profile/${attendee.username}`}>{attendee.displayname}</Link>
               </Item.Header>
               <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
             </Item.Content>
           </Item>
-
-          <Item style={{ position: "relative" }}>
-            <Image size="tiny" src={"/assets/user.png"} />
-            <Item.Content verticalAlign="middle">
-              <Item.Header as="h3">
-                <Link to={`#`}>Tom</Link>
-              </Item.Header>
-              <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
-            </Item.Content>
-          </Item>
-
-          <Item style={{ position: "relative" }}>
-            <Image size="tiny" src={"/assets/user.png"} />
-            <Item.Content verticalAlign="middle">
-              <Item.Header as="h3">
-                <Link to={`#`}>Sally</Link>
-              </Item.Header>
-            </Item.Content>
-          </Item>
+          ))}
+        
         </List>
       </Segment>
     </Fragment>

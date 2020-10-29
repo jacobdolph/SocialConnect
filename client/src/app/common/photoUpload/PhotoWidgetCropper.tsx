6a7daa58753894460192ff812e1026fd75bb3a64
Jacob Dolph <jacob.dolph702@gmail.com>
2020-10-28 21:07:30 -0600

    created the upload photo widgets

---
+++ client/src/app/common/photoUpload/PhotoWidgetCropper.tsx
@@ -0,0 +1,44 @@
+import React, { useRef } from "react";
+import "cropperjs/dist/cropper.css";
+import Cropper from "react-cropper";
+
+interface IProps {
+  setImage: (file: Blob) => void;
+  imagePreview: string;
+}
+
+const PhotoWidgetCropper: React.FC<IProps> = ({ setImage, imagePreview }) => {
+  const cropper = useRef<Cropper>(null);
+  const cropImage = () => {
+    if (
+      cropper.current &&
+      typeof cropper.current.getCroppedCanvas() === "undefined"
+    ) {
+      return;
+    }
+    cropper &&
+      cropper.current &&
+      cropper.current.getCroppedCanvas().toBlob((blob: any) => {
+        setImage(blob);
+      }, "image/jpeg");
+  };
+  return (
+    <Cropper
+      src={imagePreview}
+      style={{ height: 200, width: "100%" }}
+      // Cropper.js options
+      aspectRatio={1 / 1}
+      preview=".img-preview"
+      guides={false}
+      ref={cropper}
+      viewMode={1}
+      dragMode="move"
+      scalable={true}
+      cropBoxMovable={true}
+      cropBoxResizable={true}
+      crop={cropImage}
+    />
+  );
+};
+
+export default PhotoWidgetCropper;
